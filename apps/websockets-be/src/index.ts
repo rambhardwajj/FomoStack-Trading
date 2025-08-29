ff00d53249bf27b05dba7b359a71224fe631d1d5
Ram Bhardwaj <rambhardwaj101000@gmail.com>
2025-08-29 17:14:21 +0530

    feat: created WEbsocket server for socket connection from binance api and streaming data to redis pub sub queue

---
+++ apps/websockets-be/src/index.ts
@@ -0,0 +1,58 @@
+import { createClient } from "redis";
+import WebSocket, { WebSocketServer } from "ws";
+import {BINANCE_ASSETS} from "@repo/assets"
+
+const wss = new WebSocketServer({ port: 8080 });
+
+type User = {
+  ws: WebSocket;
+};
+const users: User[] = [];
+
+const subscriberClient = createClient({ url: "redis://localhost:6379" });
+
+async function connectClient() {
+  try {
+    await subscriberClient.connect();
+    console.log("connected to redis subs client ");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+wss.on("connection", (ws) => {
+  const user: User = { ws };
+  console.log("new user connected");
+  users.push(user);
+
+  ws.on("close", () => {
+    console.log("user disconnected");
+    const ind = users.indexOf(user);
+    if (ind != -1) users.splice(ind, 1);
+  });
+});
+
+async function subscribeToThePubSub() {
+  try {
+    console.log('in suvtothepubsub ')
+
+    for( const asset of BINANCE_ASSETS){
+      console.log(asset)
+      await subscriberClient.subscribe(`binance:prices:${asset}`, (message) => {
+        console.log("subscribed to - ", asset)
+        console.log("subscribed message - ", message);
+        users.forEach((user) => {
+          user.ws.send(message);
+        });
+      });
+
+    }
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+(async () => {
+  await connectClient();
+  await subscribeToThePubSub();
+})();
