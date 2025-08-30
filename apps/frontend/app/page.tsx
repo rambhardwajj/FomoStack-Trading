18b18d7d9980ce0cdd8a3aab10f702559c69f98e
Ram Bhardwaj <rambhardwaj101000@gmail.com>
2025-08-30 07:55:47 +0530

    feat: initalized frontend app and created asset real time tickering prices with the logic of incremental and decremental colors

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+type AssetData = {
+  buyPrice: number;
+  sellPrice: number;
+  lastBuyPrice: number;
+  lastSellPrice: number;
+};
+
+export default function Home() {
+  const socketRefVal = useRef<WebSocket | null>(null);
+  const assetsRef = useRef<Record<string, AssetData>>({});
+  const [, setRender] = useState<number>(0);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    socketRefVal.current = socket;
+
+    socket.onopen = () => {
+      console.log("connected to websocket server");
+    };
+    socket.onmessage = (event) => {
+      const msg = JSON.parse(event.data);
+      const existing = assetsRef.current[msg.symbol];
+      assetsRef.current[msg.symbol] = {
+        buyPrice: Number(msg.buyPrice) ,
+        sellPrice: Number(msg.sellPrice),
+        lastBuyPrice: existing ? existing.buyPrice : Number(msg.buyPrice),
+        lastSellPrice: existing ? existing.sellPrice : Number(msg.sellPrice),
+      };
+      // console.log(assetsRef.current)
+    };
+
+    setInterval(() => {
+      console.log("Rerendering the component");
+      setRender((prev) => prev + 1); // just trigginering the rerender
+    }, 300);
+    socket.onerror = (error) => {
+      console.error("WebSocket Error:", error);
+    };
+    socket.onclose = () => {
+      console.log("Disconnected from WebSocket server");
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">FomoStack</h1>
+      <h2 className="text-2xl font-bold underline">Real-time Crypto Prices</h2>
+      <br />
+      {Object.entries(assetsRef.current).map(
+        ([symbol, { buyPrice, sellPrice, lastBuyPrice, lastSellPrice }]) => (
+          <div
+            key={symbol}
+            className="p-3 border rounded shadow-sm flex justify-between bg-white"
+          >
+            <span className="font-semibold">{symbol}</span>
+            <div className="flex gap-4">
+              <div className={`${lastBuyPrice < buyPrice ? "text-green-600": "text-red-400"} `}>Buy: {buyPrice.toFixed(2)}</div>
+              <div className={`${lastSellPrice < sellPrice ? "text-green-600": "text-red-400"} `}>Sell: {sellPrice.toFixed(2)}</div>
+            </div>
+            <br />
+          </div>
+        )
+      )}
+    </div>
+  );
+}
